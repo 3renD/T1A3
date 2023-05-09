5c9a9ce6b82f7771dec7c2017aa0d63c27dee883
Eren <129094495+3renD@users.noreply.github.com>
2023-05-10 00:09:51 +1000

    init inputs & receipt

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-menu = {
-    "burger": 12.0,
-    "wrap": 10.5,
-    "pizza": 15.0,
-    "chips": 4.5,
-    "drink": 3.5
-    
-}
 
+
+menu = dict(
+    burger=12.0,
+    wrap=10.5,
+    pizza=15.0,
+    chips=4.5,
+    drink=3.5   
+)
+
+# print(menu)
+
+# customers = []
+# customers.append(input("what's your name? "))
+# print(customers)
+# input(f'what would you like {customers[0]}? ')
+
+name = input("Enter the name of the Customer: ")
+dish = input("Enter the dish: ")
+dish_quantity = input("Enter the dish quantity: ")
+
+total = float(menu[dish]) * float(dish_quantity)
+
+print('************************************************')
+print("Receipt of Customer: " + name)
+print("************************************************")
+print(f'{dish} ${str(menu[dish])} x {dish_quantity}')
+print("total cost: $" + str(total))
