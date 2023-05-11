5b8905b5ee8bc6e0b13c7c0e76f06dcef69a6f75
Eren <129094495+3renD@users.noreply.github.com>
2023-05-12 00:52:06 +1000

    members program

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
-
-
-menu = dict(
-    burger=12.0,
-    wrap=10.5,
-    pizza=15.0,
-    chips=4.5,
-    drink=3.5   
-)
-
-# print(menu)
-
-# customers = []
-# customers.append(input("what's your name? "))
-# print(customers)
-# input(f'what would you like {customers[0]}? ')
-
+# the program will ask the name of customer, the dish, and the quantity
 name = input("Enter the name of the Customer: ")
+# valid dishes include: hamburger, coke, pizza
 dish = input("Enter the dish: ")
-dish_quantity = input("Enter the dish quantity: ")
+quantity = input("Enter the dish quantity: ")
 
-total = float(menu[dish]) * float(dish_quantity)
+customer_list = []
+member_list = []
+# using the dictionary to store the cost of each dish
+menu = dict(coke=3.0, hamburger=12.5, pizza=14.0)
+# using append to add new customer to rewards_list
+# enter y to be added to rewards program or n to not join
+if name not in member_list:
+    member_answer = input("would you like to be in the members program?: ")
+    while member_answer != "y" and member_answer != "n":
+        member_answer = input("Please enter correct input (y/n): ")
+    if member_answer == "y":
+        member_list.append(name)
 
+line_total = float(menu[dish]) * float(quantity)
+# adding 10% fee to customer not in members program and 0% if they are
+service_fee = 0 if name in member_list else line_total * 0.10
+total = line_total + service_fee
+# This will display the receipt
 print('************************************************')
 print("Receipt of Customer: " + name)
 print("************************************************")
-print(f'{dish} ${str(menu[dish])} x {dish_quantity}')
-print("total cost: $" + str(total))
+print(f'{dish} {str(menu[dish])} (AUD) x {quantity}')
+print("Service fee: " + str(service_fee), "(AUD)")
+print("total cost: " + str(total), "(AUD)")
