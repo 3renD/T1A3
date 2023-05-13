f4796fec00733e70a9ef2f70a0446041b8d24aad
Eren <129094495+3renD@users.noreply.github.com>
2023-05-14 02:09:20 +1000

    more detailed comments

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ member_list = []
 
 #code block for option 4
 def add_dishes_prices():
+    #you can add/update dishes by typing (dish name): (price). Add/update multiple dishes by seperating them with a comma.
     dishes_prices = input("Enter new dishes and prices separated by commas (e.g chips: 4, wrap: 6): ")
     menu.update(dict([dish_price.split(": ") for dish_price in dishes_prices.split(", ")]))
 
@@ -22,7 +23,7 @@ def display_customers_info():
             member_status = "Member" if name in member_list else "Not Member"
             print(f"Name: {name}, Dish: {dish}, Quantity: {quantity}, Service Fee: {service_fee} (AUD), Total Cost: {total} (AUD), Member Status: {member_status}")
 
-#menu list
+#menu list. Choose the options below by typing the corresponidng number
 while True:
     print("Welcome to the restaurant ordering system!")
     print("Please select an option:")
@@ -37,7 +38,7 @@ while True:
 #code block for option 1
     if choice == "1":
         name = input("Enter the name of the Customer: ")
-        #dish + error handling
+        #type in valid dish + error handling
         while True:
             dish = input("Enter the dish: ")
             if dish in menu:
@@ -46,7 +47,7 @@ while True:
                 print("Invalid dish entered. Please enter a valid dish (coke, burger, pizza).")
 
 
-        # dish quantity + error handling
+        # type in dish quantity + error handling
         while True:
             try:
                 quantity = int(input("Enter the dish quantity: "))
@@ -67,7 +68,7 @@ while True:
                 member_answer = input("Please enter correct input (y/n): ")
             if member_answer == "y":
                 member_list.append(name)
-
+        #cost of food
         line_total = float(menu[dish]) * float(quantity)
         # adding 10% fee to customer not in members program and 0% if they are
         service_fee = 0 if name in member_list else line_total * 0.10
@@ -99,7 +100,7 @@ while True:
     elif choice == "4":
         add_dishes_prices()
 
-#option 5 code block
+#option 5 code block. Exit program
     elif choice == "5":
         break
 #menu option error handle
